58c73ea7de2b7349c4a643bc7048a28a599a5e17
zondimaqina <zondimaqina0@gmail.com>
2024-05-07 08:59:50 +0200

    add private methods

--- main.rb
+++ main.rb
@@ -1,10 +1,33 @@
 # class for orfanising plyers
 class Players
-    @@players = Array.new()
-    @@limit = 3
-    @@player_moves = Array.new()
-  def initialize
-    
+  def initialize(name)
+    @players = []
+    @limit = 3
+    @player_moves = []
+  
+    if @@players.length < @@limit
+      @name = name
+      @@players.push(@name)
+      player_active
+    else
+      (raise 'Only two players can play the game')
+    end
+  end
+
+  protected
+
+  def player_active
+    puts "\n#{@name} has entered the game"
+
+    if @players.length <= 1
+      @player_moves.push(choose_move)
+    else
+      if @player_moves[0] == 'X'
+        @player_moves.push('O')
+      else
+        @player_moves.push('X')
+      end
+    end
   end
 end
 
