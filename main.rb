b7eb26cf1071507fe81995c549e6371765285472
zondimaqina <zondimaqina0@gmail.com>
2024-05-07 09:25:13 +0200

    add game board method

--- main.rb
+++ main.rb
@@ -48,9 +48,35 @@ end
 # class for playing game
 class Game < Players
   def initialize
+    @position = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    @columns1 = Array.new()
+    @columns2 = Array.new()
+    @columns3 = Array.new()
+
+    @rows1 = Array.new()
+    @rows2 = Array.new() 
+    @rows3 = Array.new() 
+
+    @diagonal1 = Array.new()
+
+    show_board
+    play_rounds
+  end
+
+  private
+
+  def show_board
+    puts '---------------------'
+    puts " #{@position[0]} | #{@position[1]} | #{@position[2]}"
+    puts "---+---+---"
+    puts " #{@position[3]} | #{@position[4]} | #{@position[5]}"
+    puts "---+---+---"
+    puts " #{@position[6]} | #{@position[7]} | #{@position[8]}"
+    puts '---------------------'
   end
 end
 
+puts "Welcome to Tic-Tac-Toe"
 puts "\nEnter your name player 1"
 name_one = gets.chomp
 Players.new(name_one)
